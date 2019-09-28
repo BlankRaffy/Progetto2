e0372aeb62873131a733dabf47b3fd63af5451df
raffa <raffa@DESKTOP-OUINP3S.homenet.telecomitalia.it>
2019-09-28 12:49:11 +0200

    mannag a tutt cos

--- src/controller/AdminDeleteProdottiServlet.java
+++ src/controller/AdminDeleteProdottiServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import dao.ProdottoDAO;
 
-@WebServlet("/AdminModificaProdottiServlet")
+@WebServlet("/")
 public class AdminDeleteProdottiServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
