ee1ea4e87ba2d7f0fbb854141b5d9f86ce1bef60
raffa <raffa@DESKTOP-OUINP3S.homenet.telecomitalia.it>
2019-09-29 19:43:52 +0200

    COMM A TE'

--- src/controller/CarrelloDeleteServlet.java
+++ src/controller/CarrelloDeleteServlet.java
@@ -13,44 +13,36 @@ import javax.servlet.http.HttpSession;
 import dao.ProdottoDAO;
 import model.ProdottoBean;
 
-
 @SuppressWarnings("serial")
 @WebServlet("/CarrelloDeleteServlet")
 
 public class CarrelloDeleteServlet extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-             
-		
-		int id =  Integer.parseInt(request.getParameter("id"));
 
-        
-       ProdottoDAO a = new ProdottoDAO();
-       System.out.println(id);
-       
-       
-         HttpSession session = request.getSession();
-        	 ArrayList <ProdottoBean> carrello = (ArrayList<ProdottoBean>) request.getAttribute("carrello");
-        	 ProdottoBean c =     a.GetProdotto(id);
-        	 carrello.remove(0);
-        	 
-        	 
-        	 session.setAttribute("carrello", carrello);
-        	 
-         RequestDispatcher rd = request.getRequestDispatcher("Carrello.jsp");
- 		rd.forward(request, response);
-        	 
-         
+		int id = Integer.parseInt(request.getParameter("id"));
 
-         
-         
+		ProdottoDAO a = new ProdottoDAO();
+		System.out.println(id);
+
+		HttpSession session = request.getSession();
+		ArrayList<ProdottoBean> carrello = (ArrayList<ProdottoBean>) request.getAttribute("carrello");
+		ProdottoBean c = a.GetProdotto(id);
+		for (int i = 0; i < carrello.size(); i++) {
+			if (carrello.get(i).getCodice() == c.getCodice()) {
+				carrello.remove(i);
+
+			}
+		}
+
+		session.setAttribute("carrello", carrello);
+
+		RequestDispatcher rd = request.getRequestDispatcher("Carrello.jsp");
+		rd.forward(request, response);
+
 	}
-	
-	
-	
-	
+
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
-
 
 }
