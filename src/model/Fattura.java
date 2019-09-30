5b0c40c916d3a1b355f256ffa4001bf83e6a269b
raffa <raffa@172.19.13.167>
2019-09-30 09:51:53 +0200

    g

---
+++ src/model/Fattura.java
@@ -0,0 +1,17 @@
+package model;
+import java.util.*;
+
+public class Fattura {
+	public Fattura() {
+		// TODO Auto-generated constructor stub
+	}
+	
+   public ArrayList<Fattura> addFattura(){
+	   return null;
+   }
+   
+   
+   private String Email ;
+   private ProdottoBean prodotto;
+
+}
