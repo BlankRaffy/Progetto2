bbdbb8ab00ab4e8ab1fd71f40843432455661490
raffa <raffa@DESKTOP-OUINP3S.homenet.telecomitalia.it>
2019-10-06 12:37:00 +0200

    script utente

---
+++ db/utenti.sql
@@ -0,0 +1,3 @@
+insert into farmacia.utente (Nome,Cognome,Telefono,Password,Email) values ('Gaetano','Ruggiero',3334162638,'gaetano','teostraoneshot@gmail.com');
+insert into farmacia.utente (Nome,Cognome,Telefono,Password,Email) values ('Raffaele','Ferriero',1234567890,'123456','raffy@gmail.com');
+
